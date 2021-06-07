9db93484ac10a61b9b4520b7fa0fb64422277830
Harshad Kadam <48818400+HarshadKadam1998@users.noreply.github.com>
2021-06-07 13:37:16 +0530

    Create ExtraLongFactorials.java

---
+++ ExtraLongFactorials.java
@@ -0,0 +1,46 @@
+/*
+Extra Long Factorials:
+
+The problem-statement can be accessed using following link --->
+https://www.hackerrank.com/challenges/extra-long-factorials/problem
+*/
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.math.BigInteger;
+class Result {
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n) {
+        BigInteger ans=new BigInteger("1");
+        while(n>=1){
+            ans= ans.multiply(BigInteger.valueOf(n));
+            n--;
+        }
+        System.out.println(ans);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
